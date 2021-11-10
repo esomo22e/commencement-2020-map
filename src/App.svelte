02275a5fc198e67b0240d674a4cd5dc52c14e496
Eunice Esomonu <eesomonu@gmail.com>
2021-11-10 17:11:15 -0500

    fix styling on the page

--- src/App.svelte
+++ src/App.svelte
@@ -95,12 +95,13 @@ onMount(() => {
 	let nextButton = document.querySelector('.nu-map-button--next');
 	// const mapWrapper = document.querySelector('.map-wrapper');
 	let canvasElement = document.querySelector('#nu-map-effects');
-	// const canvasPath = canvasElement.getContext('2d');
+	const canvasPath = canvasElement.getContext('2d');
 	let minimumParticles = 5;
 	let colors = ['#badb00', '#00cfb5', '#ff854f', '#ffbf3d', '#006eb5'];
 	let particleInterval;
 	let _particles = [];
 	let activeLocation = 0;
+    let tl = gsap.timeline({ease: Power2.in});
 
 	let wrapperBounds = document.querySelector('.map-wrapper').getBoundingClientRect();
 	let graphicBounds = document.querySelector('.map-graphic').getBoundingClientRect();
@@ -179,12 +180,105 @@ onMount(() => {
 		}
 
 		// TODO: Add navTo(index) call here.
-		navTo(locations[index].id);
+
+		navTo(index+1);
+
 		activeLocation = index;
 		let location = locations[index];
 		locationInfoTitle.innerHTML = location.name;
 		locationInfoText.innerHTML = location.description;
 	}
+
+	class Particles {
+		   constructor(x, y, pSize, color) {
+			   this.x = x
+			   this.y = y
+			   this.size = pSize
+			   this.color = color
+			   this.mass = 5 * Math.random() - 2.5;
+			   this.dir = -1 * Math.random();
+
+		   }
+
+		   draw() {
+			   canvasPath.beginPath()
+			   if (0 > this.size) {
+				   this.size = -1 * this.size;
+			   }
+			   canvasPath.arc(this.x, this.y, this.size, 0, Math.PI * 2)
+			   canvasPath.fillStyle = this.color
+			   canvasPath.fill()
+
+		   }
+
+		   update() {
+			   this.y -= this.mass
+			   this.x += this.dir
+			   if (this.size > 0.1) {
+				   this.size -= 0.2
+			   }
+
+		   }
+	   }
+
+	   function click() {
+	        let boundingClientRect = this.getBoundingClientRect();
+
+	        particleInterval = setInterval(() => {
+	            for (let i = 0; i < minimumParticles; i++) {
+	                // let x = boundingClientRect.x - (boundingClientRect.width / 2) + Math.random() * (40 - 40) + 40;
+	                let x = boundingClientRect.x + (boundingClientRect.width / 2);
+	                let y = boundingClientRect.y - (boundingClientRect.height / 2) + 50;
+	                let color = colors[Math.floor(Math.random() * colors.length)];
+	                let size = Math.random() * 20 + 10
+	                _particles.push(new Particles(x, y, size, color))
+	            }
+	        }, 60);
+
+	        tl.to(this,
+	            {
+	                scale: 0.8,
+	                duration: 0.05
+	            })
+	            .to(this,
+	                {
+	                    scale: 1,
+	                    duration: 0.05,
+	                    onComplete: () => {
+	                        clearInterval(particleInterval)
+	                    }
+	                })
+
+	    }
+
+		function spawnParticles() {
+	         _particles.forEach((particle, index) => {
+
+	             particle.draw()
+	             particle.update()
+
+	             if (particle.pSize < 1) {
+	                 _particles.splice(index, 1)
+
+
+	             }
+	         })
+	     }
+
+
+	     function animateParticles() {
+	         canvasPath.clearRect(0, 0, canvasElement.width, canvasElement.height)
+
+	         nextButton.addEventListener('click', click)
+	         prevButton.addEventListener('click', click)
+
+
+	         spawnParticles()
+	         requestAnimationFrame(animateParticles)
+	     }
+
+	     animateParticles()
+
 
 
 });
@@ -209,10 +303,156 @@ function navTo(index) {
 
 
 
-
 </script>
 
 <style>
+
+:root{
+	/**
+	   * Global font jobs.
+	   * Fonts should be set them to the job the text is doing. Avoid assigning
+	   * specific font faces on elements to keep future updates simple.
+	   *
+	   * Body. Versatile and facilitates reading even long-form content comfortably.
+	   *
+	   * Display. To attract attention and set the tone. Good for top-level headings
+	   * and hero text.
+	   *
+	   * Lettering. Artistic font for displaying an individual letter with flair.
+	   *
+	   * Signage. To help people find their way around. Use for labels in a grid of
+	   * products, buttons, strong calls to action, and footers full of links.
+	   *
+	   * Small. For clarity at small sizes. Captions, labels, footnotes, or where
+	   * there is limited screen real estate.
+	   *
+	   * Tabular. Quickly scannable to gather data and make comparisons. Ideal for
+	   * tables of contents, charts, or anywhere data needs to be easily understood.
+	   *
+	   * Technical. For technical accuracy where glyph placement is crucial. Use
+	   * when displaying math formulas, code, or even poetry and infographics.
+	   *
+	   * @since 1.5
+	   */
+	   --global--font-body: harriet, serif;
+	   --global--font-display: akkurat, sans-serif;
+	   --global--font-display-impact: trim-poster, sans-serif;
+	   --global--font-signage: brauerneue, sans-serif;
+	   --global--font-small: akkurat, sans-serif;
+	   --global--font-tabular: akkurat, sans-serif;
+	   --global--font-technical: akkurat, sans-serif;
+	   --global--font-lettering: 'Pinyon Script', cursive;
+
+	   /**
+ * Global font sizes.
+ * Specified in t-shirt sizing.
+ *
+ * @since 1.5
+ */
+--global--font-size-xxs: 12px; /* 12px */
+--global--font-size-xs: 0.875rem; /* ~14px */
+--global--font-size-s: 1rem; /* ~16px */
+--global--font-size-m: 1.188rem; /* ~19px */
+--global--font-size-l: 1.5rem; /* ~24px */
+--global--font-size-xl: 1.875rem; /* ~30px */
+--global--font-size-xxl: 2.25rem; /* ~36px */
+--global--font-size-3xl: 2.5rem; /* 40px */
+--global--font-size-4xl: 3rem; /* 48px */
+--global--font-size-5xl: 4.063rem; /* ~65px */
+--global--font-size-6xl: 4.5rem; /* ~72px */
+/* Line Height */
+--global--line-height-body: 1.4;
+/* Content bounds */
+--global--ceiling-text: 700px;
+--global--ceiling-image: 750px;
+--global--ceiling-content: 1150px;
+
+/**
+	  * Global font weights.
+	  *
+	  * @since 1.5
+	  */
+	 --global--font-weight-thin: 100;
+	 --global--font-weight-extra-light: 200;
+	 --global--font-weight-light: 300;
+	 --global--font-weight-regular: 400;
+	 --global--font-weight-medium: 500;
+	 --global--font-weight-semi-bold: 600;
+	 --global--font-weight-bold: 700;
+	 --global--font-weight-extra-bold: 800;
+	 --global--font-weight-black: 900;
+	 --global--font-weight-ultra-black: 950;
+	 /**
+	  * Colors.
+	  * The names for hues are those found on a basic RYB color wheel with tertiary
+	  * colors.
+	  *
+	  * @see https://en.wikipedia.org/wiki/Tertiary_color#Traditional_painting_(RYB)
+	  *
+	  * Red, Red-Orange, Orange, Yellow-Orange, Yellow, Yellow-Green, Green,
+	  * Blue-Green, Blue, Blue-Violet, Violet, and Red-Violet, plus Gray, White,
+	  * and Black.
+	  *
+	  * Accessibility:
+	  * White text will pass on colors with a "dark-" prefix.
+	  * Black text will pass on colors with a "light-" prefix.
+	  *
+	  * Neutral colors:
+	  * Neutral colors that are not true neutral have either a "warm" or "cool"
+	  * prefix, as appropriate.
+	  */
+	 --global--color-darkest-red: #1f0406;
+	 --global--color-dark-red: #7a0f19;
+	 --global--color-red: #D41B2C;
+	 --global--color-darker-blue: #1b3645;
+	 --global--color-dark-blue: #385775;
+	 --global--color-blue: #006EB5;
+	 --global--color-light-blue: #9ebcda;
+	 --global--color-lighter-blue: #f7fcfd;
+	 --global--color-accent-blue: #52CFE5;
+	 --global--color-blue-green: #00CFB5;
+	 --global--color-violet: #6e016b;
+	 --global--color-darkest-blue: #1b3645;
+	 --global--color-teal: #00cfb5;
+	 --global--color-purple: #6e016b;
+	 --global--color-black: #000;
+	 --global--color-white: #fff;
+	 --global--color-darker-gray: #222;
+	 --global--color-dark-gray: #6F7680;
+	 --global--color-gray: #555;
+	 --global--color-light-gray: #99a3b0;
+	 --global--color-lighter-gray: #cbcccb;
+	 --global--color-lightest-gray: #efefef;
+	 --global--color-primary: var(--global--color-black);
+	 --global--color-background: var(--global--color-white);
+	 --global--color-secondary: var(--global--color-red);
+	 --global--color-disabled: var(--global--color-lighter-gray);
+	 --global--color-disabled-background: var(--global--color-lightest-gray);
+
+	 /**
+		* Global Spacing.
+		*/
+	   --global--spacing-unit: 16px;
+	   --global--spacing-vertical: calc(5 * var(--global--spacing-unit));
+	   --global--spacing-horizontal: calc(2.5 * var(--global--spacing-unit));
+	   --global--spacing-gap: 20px;
+	   --global--spacing-extra-small: calc(0.6 * var(--global--spacing-unit));
+	   --global--spacing-small: calc(0.8 * var(--global--spacing-unit));
+	   --global--spacing-medium: var(--global--spacing-unit);
+	   --global--spacing-large: calc(1.5 * var(--global--spacing-unit));
+	   --global--spacing-extra-large: calc(1.75 * var(--global--spacing-unit));
+	   /**
+	   * Global Borders
+	   */
+	   --global--border-color-light: var(--global--color-lightest-gray);
+	   --global--border-color-regular: var(--global--color-lighter-gray);
+	   --global--border-color-heavy: var(--global--color-gray);
+	   --global--border-weight-regular: 1px;
+	   --global--border-weight-bold: 2px;
+	   --global--border-radius-tight: 3px;
+	   --global--border-radius-regular: 8px;
+	   --global--border-radius-loose: 16px;
+}
 
 :root {
   --nu-map--color--transparent-gray: rgba(211, 203, 203, 0.92);
@@ -467,7 +707,32 @@ background-color: #E5D4AB;
   .nu-map-button + .nu-map-button {
     margin-left: var(--global--spacing-small);
   }
+
+  .map-wrapper{
+	  width: 100%;
+  }
 }
+
+/* TODO: Missing media query for large viewport */
+@media only screen and (min-width: 1008px) {
+	:root {
+             --nu-map-button-icon--size: 36px;
+             --nu-map-navigation--width: var(--nu-map-location-info--width);
+         }
+         .nu-map-location-info {
+             border-radius: 0 0 0 var(--global--border-radius-regular);
+             right: 0;
+             top: calc(var(--global--spacing-medium) + var(--nu-map-button-icon--size));
+         }
+         .nu-map-location-info__toggle {
+             display: none;
+         }
+         .nu-map-location-title {
+             padding: 0 var(--global--spacing-medium);
+             text-align: center;
+         }
+}
+
 
 </style>
 <!-- <main> -->
@@ -521,11 +786,11 @@ background-color: #E5D4AB;
     </div>
 
 </div>
-
+<!--
 {#each locations as location}
 
 <div class = "location-name" on:click={() => navTo(location.id)}>{location.name}</div>
-{/each}
+{/each} -->
 <!-- <div class = "location-name" on:click={() => navTo(3)}>location 3</div> -->
 <!-- <div class = "location-name" on:click={() => navTo(5)}>location 5</div> -->
 
